81ac0ae0de5fddeb8fbea9253a8d5420d8b20b84
Royston <roystonad2004@gmail.com>
2024-11-22 09:34:20 +0530

    like and update feature

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.get("/profile",isLoggedIn,async (req,res)=>{
     //populate the user with the created posts
     let user = await userModel.findOne({email:req.user.email}).populate("post");
     
-    res.render("profile",{user:user})
+    res.render("profile",{user})
 })
 
 app.post("/post",isLoggedIn,async (req,res)=>{
@@ -85,11 +85,41 @@ app.post("/post",isLoggedIn,async (req,res)=>{
     res.redirect("/profile")
 })
 
+app.get("/like/:id",isLoggedIn,async (req,res)=>{
+    // we are populating in order to load the field withh data rather than just the id
+    let post = await postModel.findById(req.params.id).populate("user")
+    
+    if(post.likes.indexOf(req.user.userid)=== -1){
+        post.likes.push(req.user.userid)
+        
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid),1);
+    }
+   
+    await post.save()
+    res.redirect("/profile");
+})
+
+app.get("/edit/:id",isLoggedIn,async (req,res)=>{
+    // we are populating in order to load the field withh data rather than just the id
+    let post = await postModel.findOne({_id:req.params.id}).populate("user")
+    
+    res.render("edit",{post})
+    
+})
+
+app.post("/update/:id",isLoggedIn,async function(req,res){
+    let post = await postModel.findOneAndUpdate({_id:req.params.id},{content:req.body.content})
+    res.redirect("/profile")
+})
+
 function isLoggedIn(req,res,next){
     if(req.cookies.token === "") res.redirect("/login");
     else{
         let data = jwt.verify(req.cookies.token,"Secret")
         req.user = data;
+        console.log(req.user)
         next();
     }
     
